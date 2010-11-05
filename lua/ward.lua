47ccf00e3e4b88a98ff89d8ce2bae7808c6718f4
"Reimer Behrends ext:(%22) <behrends@gmail.com>
2010-11-05 08:33:57 +0000

    Added basic option parsing.

--- lua/ward.lua
+++ lua/ward.lua
@@ -7,10 +7,34 @@ require "cparser"
 require "dataflow"
 
 local files_parsed = 0
+options = {
+  report_type = "errors",
+  pass_count = 2,
+  parse_only = false,
+}
 
-for i = 1, #arg do
+local i = 1
+while i <= #arg do
   local file = arg[i]
+  i = i + 1
   if string.sub(file, 1, 1) == "-" then
+    if file == "-pass" then
+      local pass_count = arg[i]
+      i = i + 1
+      if pass_count and lpeg.match(lpeg.R("09")^1*lpeg.P(-1), pass_count) then
+        options.pass_count = tonumber(pass_count)
+      else
+        system_error("The -pass option requires a numeric argument")
+      end
+    elseif file == "-errors" then
+      options.report_type = "errors"
+    elseif file == "-suggest" then
+      options.report_type = "suggestions"
+    elseif file == "-parseonly" then
+      options.parse_only = true
+    else
+      system_error("Unknown option '" .. file .. "'")
+    end
   else
     local command = {"gcc", "-E"}
     for _, opt in ipairs(preprocessor_options) do
@@ -34,7 +58,7 @@ for i = 1, #arg do
 end
 
 
-if files_parsed > 0 then
+if files_parsed > 0 and not options.parse_only then
   run_dataflow_analysis()
 end
 
