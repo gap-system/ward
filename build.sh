b64811e6bec6a244e3dd154a98a6bdaec784233f
"Reimer Behrends ext:(%22) <behrends@gmail.com>
2011-02-14 11:25:05 +0000

    Upgraded LuaJIT to beta6, added addguards2c/addguardsc scripts.

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ case `uname -s` in
 esac
 cd $LUA_ROOT/ext/lua-5.1.4
 make INSTALL_TOP=$LUA_ROOT $PLATFORM install
-cd $LUA_ROOT/ext/LuaJIT-2.0.0-beta3
+cd $LUA_ROOT/ext/LuaJIT-2.0.0-beta6
 make PREFIX=$LUA_ROOT install
 cd $LUA_ROOT/bin
-ln -s luajit-2.0.0-beta3 luajit2
+ln -sf luajit-2.0.0-beta6 luajit2
 export PATH=$LUA_ROOT/bin:$PATH
 cd $LUA_ROOT/ext/luarocks-2.0.2
 ./configure
@@ -43,6 +43,12 @@ export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUA_PATH'
 "$LUA_ROOT/bin/lua" "$LUA_ROOT/lua/addguards.lua" "\$@"
 EOF
 chmod 755 bin/addguards
+cat >bin/addguardsc <<EOF
+#!/bin/sh
+export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUA_PATH'
+"$LUA_ROOT/bin/luajit2" "$LUA_ROOT/lua/addguards.lua" "\$@"
+EOF
+chmod 755 bin/addguardsc
 cat >bin/addguards2 <<EOF
 #!/bin/sh
 export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUA_PATH'
@@ -50,4 +56,11 @@ export WARD=$LUA_ROOT
 "$LUA_ROOT/bin/lua" "$LUA_ROOT/lua/addguards2.lua" "\$@"
 EOF
 chmod 755 bin/addguards2
+cat >bin/addguards2c <<EOF
+#!/bin/sh
+export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUA_PATH'
+export WARD=$LUA_ROOT
+"$LUA_ROOT/bin/luajit2" "$LUA_ROOT/lua/addguards2c.lua" "\$@"
+EOF
+chmod 755 bin/addguards2c
 chmod 755 bin/findguards
