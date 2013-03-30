353ef6be96a967c5b2ee438247fed2c7d7750237
Reimer Behrends <behrends@gmail.com>
2013-03-30 16:53:50 +0000

    Updated to a new build system.
    Lua and Lpeg are now built using SCons.
    The new build process also works on Cygwin.
    Lua and Lpeg also have been updated to newer versions.

--- build.sh
+++ build.sh
@@ -1,77 +1,65 @@
-#!/bin/sh
-export LUA_ROOT=`pwd`
-case `uname -s` in
-  Darwin)
-    export PLATFORM=macosx
-    ;;
-  Linux)
-    export PLATFORM=linux
-    ;;
-  *)
-    export PLATFORM=posix
-    ;;
-esac
-test -e .ward_directory && rm -rf build
-test -e .ward_directory && cp -pr ext build
-cd $LUA_ROOT/build/lua-5.1.4
-make INSTALL_TOP=$LUA_ROOT $PLATFORM install
-export PATH=$LUA_ROOT/bin:$PATH
-cd $LUA_ROOT/build/LuaJIT-2.0.0
-if make PREFIX=$LUA_ROOT install; then
-  cd $LUA_ROOT/bin
-  ln -sf luajit-2.0.0 luajit2
-  LUAJIT_OPT=" -jon"
-else
-  cd $LUA_ROOT/bin
-  ln -sf lua luajit2
-  LUAJIT_OPT=""
-fi;
-cd $LUA_ROOT/build/luarocks-2.0.2
-./configure
-make install
-cd $LUA_ROOT/build/lpeg
-luarocks make lpeg-local-1.rockspec && cp lpeg.so $LUA_ROOT/lib/lua/5.1
-cd $LUA_ROOT
-cp -f build/luarocks-2.0.2/src/bin/luarocks-orig build/luarocks-2.0.2/src/bin/luarocks
-cp -f build/luarocks-2.0.2/src/bin/luarocks-admin-orig build/luarocks-2.0.2/src/bin/luarocks-admin
-LUA_PATH=`bin/lua -e 'print(package.path)'`
-cat >bin/ward <<EOF
-#!/bin/sh
-export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUA_PATH'
-"$LUA_ROOT/bin/lua" "$LUA_ROOT/lua/ward.lua" "\$@"
-EOF
-chmod 755 bin/ward
-LUAJIT_PATH=`bin/luajit2 -e 'print(package.path)'`
-cat >bin/cward <<EOF
-#!/bin/sh
-export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUAJIT_PATH'
-"$LUA_ROOT/bin/luajit2"$LUAJIT_OPT "$LUA_ROOT/lua/ward.lua" "\$@"
-EOF
-chmod 755 bin/cward
-cat >bin/addguards <<EOF
-#!/bin/sh
-export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUA_PATH'
-"$LUA_ROOT/bin/lua" "$LUA_ROOT/lua/addguards.lua" "\$@"
-EOF
-chmod 755 bin/addguards
-cat >bin/addguardsc <<EOF
-#!/bin/sh
-export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUAJIT_PATH'
-"$LUA_ROOT/bin/luajit2" "$LUA_ROOT/lua/addguards.lua" "\$@"
-EOF
-chmod 755 bin/addguardsc
-cat >bin/addguards2 <<EOF
-#!/bin/sh
-export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUA_PATH'
-export WARD=$LUA_ROOT
-"$LUA_ROOT/bin/lua" "$LUA_ROOT/lua/addguards2.lua" "\$@"
-EOF
-chmod 755 bin/addguards2
-cat >bin/addguards2c <<EOF
-#!/bin/sh
-export LUA_PATH='$LUA_ROOT/lua/?.lua;$LUAJIT_PATH'
-export WARD=$LUA_ROOT
-"$LUA_ROOT/bin/luajit2" "$LUA_ROOT/lua/addguards2c.lua" "\$@"
-EOF
-chmod 755 bin/addguards2c
-chmod 755 bin/findguards
+#!/usr/bin/env python
+"exec" "python" "$0" "$@"
+
+import sys, os, shutil
+
+def sh(cmd):
+  print "SH: " + cmd
+  if os.system(cmd):
+    print "Command failed: " + cmd
+    sys.exit(1)
+
+root = os.getcwd()
+
+if not os.path.exists(".ward_directory"):
+  print "Must run " + __file__ + " from ward's main directory."
+  sys.exit(1)
+
+# Copy everything to a separate build directory
+sh("cp -prf ext build")
+
+# Build and install Lua
+sh('scons/scons -j 4 -C build/lua-5.1.5 prefix="'+root+'/lua" install')
+
+# Build and install LuaJIT
+sh('make -j 4 -C build/LuaJIT-2.0.0 PREFIX="'+root+'/luajit" install')
+if sys.platform.startswith("cygwin"):
+  shutil.copy2("build/LuaJIT-2.0.0/src/cyglua51.dll",
+      "luajit/bin/")
+
+# Build and install lpeg for both Lua and LuaJIT.
+sh('scons/scons -j 4 -C build/lpeg-0.11 prefix="'+root+'/lua" install')
+sh('scons/scons -j 4 -C build/lpeg-0.11 luajit=yes prefix="'+root+'/luajit" install')
+
+commands = [
+  "ward", "cward", "addguards", "addguardsc", "addguards2", "addguards2c"
+]
+
+# If the directory exists, avoid aborting.
+sh('mkdir bin || true')
+
+for cmd in commands:
+  luajit = False
+  if cmd.startswith("c"):
+    luajit = True
+    lua = cmd[1:] + ".lua"
+  elif cmd.endswith("c"):
+    luajit = True
+    lua = cmd[:-1] + ".lua"
+  else:
+    lua = cmd + ".lua"
+  with open("bin/" + cmd, "w") as cmdfile:
+    # Will be a shell file.
+    cmdfile.write("#!/bin/sh\n")
+    # So that we know where we are.
+    cmdfile.write("export WARD='" + root + "'\n")
+    if luajit:
+      path = root + "/luajit/bin/luajit"
+    else:
+      path = root + "/lua/bin/lua"
+    # Call Lua/LuaJIT with an expanded path and the lua
+    # file as an argument.
+    cmdfile.write(path + " -e 'package.path = \"" +
+      root + "/ward/?.lua;\" .. package.path' " +
+      "'" + root + "/ward/" + lua + "' \"$@\"\n")
+  os.chmod("bin/" + cmd, 0755)
--- ext/lpeg-list/sblp2009/ometa/driver.js
+++
@@ -1,5 +0,0 @@
-load("exps2.js")
-load("ometa-rhino.js")
-load("calc.js")
-load("calc2.js")
-
