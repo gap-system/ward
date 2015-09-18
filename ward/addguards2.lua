4c35b5c25430692e828e21e925093f5ed9870bb2
Reimer Behrends <behrends@gmail.com>
2015-09-18 09:42:53 +0200

    Record parse errors in generated output file.

--- ward/addguards2.lua
+++ ward/addguards2.lua
@@ -1,12 +1,23 @@
+function catfile(file)
+  local fp = io.open(file)
+  local contents = fp:read("*a")
+  fp:close()
+  io.write(contents)
+end
+
 ward = os.getenv("WARD")
 args = table.concat({...}, " ")
 argtable = {...}
 file = argtable[#argtable]
 if string.sub(file, -2, -1) == ".c" then
   guard = os.tmpname()
-  os.execute(ward .. "/bin/ward -suggest " .. args .. ">" .. guard)
-  os.execute(ward .. "/bin/addguards " .. guard .. " " .. file)
+  if os.execute(ward .. "/bin/ward -suggest " .. args .. ">" .. guard) == 0 then
+    os.execute(ward .. "/bin/addguards " .. guard .. " " .. file)
+  else
+    io.write("/* ERROR: Ward failed to parse C source */\n")
+    catfile(file)
+  end
   os.remove(guard)
 else
-  os.execute("cat "..file)
+  catfile(file)
 end
